904cb8df8837cf8082c3b80f77191682a9eb8a94
andre-paraense-mobwise <andre.paraense@mobwise.com.br>
2014-09-19 19:49:30 -0300

    BUG - Facebook sharing dialog

--- Code/src/org/vocefiscal/activities/FiscalizacaoConcluidaActivity.java
+++ Code/src/org/vocefiscal/activities/FiscalizacaoConcluidaActivity.java
@@ -1,9 +1,5 @@
 package org.vocefiscal.activities;
 
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.List;
-
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.vocefiscal.R;
@@ -25,20 +21,22 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.view.View.OnLongClickListener;
 import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.facebook.FacebookException;
+import com.facebook.FacebookOperationCanceledException;
 import com.facebook.FacebookRequestError;
 import com.facebook.HttpMethod;
-import com.facebook.LoggingBehavior;
 import com.facebook.Request;
 import com.facebook.RequestAsyncTask;
 import com.facebook.Response;
 import com.facebook.Session;
-import com.facebook.SessionState;
-import com.facebook.Settings;
+import com.facebook.UiLifecycleHelper;
+import com.facebook.widget.FacebookDialog;
+import com.facebook.widget.WebDialog;
+import com.facebook.widget.WebDialog.OnCompleteListener;
 
 public class FiscalizacaoConcluidaActivity extends AnalyticsActivity 
 {
@@ -52,8 +50,6 @@ public class FiscalizacaoConcluidaActivity extends AnalyticsActivity
 	private String zonaEleitoral;
 	private String municipio;
 
-	//Facebook Login
-	private Session.StatusCallback statusCallback;
 	private ImageButton facebookLogin;	
 
 	//Twitter Variables
@@ -62,6 +58,8 @@ public class FiscalizacaoConcluidaActivity extends AnalyticsActivity
 	private Handler handler;	
 	
 	private CustomDialogClass envio;
+	
+	private UiLifecycleHelper uiHelper;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) 
@@ -69,6 +67,9 @@ public class FiscalizacaoConcluidaActivity extends AnalyticsActivity
 		super.onCreate(savedInstanceState);
 
 		setContentView(R.layout.activity_fiscalizacao_concluida);
+		
+		uiHelper = new UiLifecycleHelper(this, null);
+	    uiHelper.onCreate(savedInstanceState);
 
 		/*
 		 * Captando a missão
@@ -118,34 +119,58 @@ public class FiscalizacaoConcluidaActivity extends AnalyticsActivity
 		{
 			@Override
 			public void onClick(View v) 
-			{							        
-				//Pega a sessão ativa
-				Session session = Session.getActiveSession();
-
-				if (session!=null&&!session.isOpened() && !session.isClosed()) 
+			{			
+				if (FacebookDialog.canPresentShareDialog(getApplicationContext(), FacebookDialog.ShareDialogFeature.SHARE_DIALOG)) 
 				{
-					session.openForRead(new Session.OpenRequest(FiscalizacaoConcluidaActivity.this).setCallback(statusCallback));
-				} else
+					FacebookDialog shareDialog = new FacebookDialog.ShareDialogBuilder(FiscalizacaoConcluidaActivity.this).setLink("http://www.vocefiscal.org")
+							.setPicture("http://imagizer.imageshack.us/v2/150x100q90/913/bAwPgx.png").setName("Você Fiscal")
+							.setDescription("Eu fiscalizei a seção: "+ FiscalizacaoConcluidaActivity.this.secao +"\nNa zona eleitoral: " + FiscalizacaoConcluidaActivity.this.zonaEleitoral + "\nNo município de: " + FiscalizacaoConcluidaActivity.this.municipio)
+							.setCaption("Obrigado por contribuir com a democracia!").build();		
+					
+					uiHelper.trackPendingDialogCall(shareDialog.present());
+				}else
 				{
-					Session.openActiveSession(FiscalizacaoConcluidaActivity.this, true, statusCallback);					
-				}
-			}
-		});
+					 Bundle params = new Bundle();
+					    params.putString("name", "Você Fiscal");
+					    params.putString("caption", "Obrigado por contribuir com a democracia!");
+					    params.putString("description", "Eu fiscalizei a seção: "+ FiscalizacaoConcluidaActivity.this.secao +"\nNa zona eleitoral: " + FiscalizacaoConcluidaActivity.this.zonaEleitoral + "\nNo município de: " + FiscalizacaoConcluidaActivity.this.municipio);
+					    params.putString("link", "http://www.vocefiscal.org");
+					    params.putString("picture", "http://imagizer.imageshack.us/v2/150x100q90/913/bAwPgx.png");
 
-		facebookLogin.setOnLongClickListener(new OnLongClickListener()
-		{			
-			@Override
-			public boolean onLongClick(View v) 
-			{
-				Session session = Session.getActiveSession();
+					    WebDialog feedDialog = (
+					        new WebDialog.FeedDialogBuilder(FiscalizacaoConcluidaActivity.this, Session.getActiveSession(),  params))
+					        .setOnCompleteListener(new OnCompleteListener() 
+					        {
+					            @Override
+					            public void onComplete(Bundle values, FacebookException error) 
+					            {
+					                if (error == null) 
+					                {
+					                    // When the story is posted, echo the success
+					                    // and the post Id.
+					                    final String postId = values.getString("post_id");
+					                    if (postId != null) 
+					                    {
+					                        Toast.makeText(FiscalizacaoConcluidaActivity.this, "Compartilhado com sucesso!",  Toast.LENGTH_SHORT).show();
+					                    } else 
+					                    {
+					                        // User clicked the Cancel button
+					                        Toast.makeText(FiscalizacaoConcluidaActivity.this, "Publicação cancelada",  Toast.LENGTH_SHORT).show();
+					                    }
+					                } else if (error instanceof FacebookOperationCanceledException) 
+					                {
+					                    // User clicked the "x" button
+					                    Toast.makeText(FiscalizacaoConcluidaActivity.this.getApplicationContext(),  "Publicação canceladad",   Toast.LENGTH_SHORT).show();
+					                } else {
+					                    // Generic, ex: network error
+					                    Toast.makeText(FiscalizacaoConcluidaActivity.this.getApplicationContext(), "Erro na publicação com Facebook",  Toast.LENGTH_SHORT).show();
+					                }
+					            }
 
-				if (session!=null&&!session.isClosed()) 
-				{
-					session.closeAndClearTokenInformation();
-				}
-
-				finish();
-				return true;
+					        })
+					        .build();
+					    feedDialog.show();
+				}				
 			}
 		});
 
@@ -248,25 +273,6 @@ public class FiscalizacaoConcluidaActivity extends AnalyticsActivity
 
 		TextView municipio = (TextView)findViewById(R.id.municipio);
 		municipio.setText(this.municipio);
-
-		statusCallback = new SessionStatusCallback();
-
-		Settings.addLoggingBehavior(LoggingBehavior.INCLUDE_ACCESS_TOKENS);
-
-		Session session = Session.getActiveSession();
-		if (session == null) 
-		{
-			if (savedInstanceState != null) 
-			{
-				session = Session.restoreSession(this, null, statusCallback, savedInstanceState);
-			}
-			else if (session == null) 
-			{
-				session = new Session(this);
-			}
-
-			Session.setActiveSession(session);
-		}
 	}
 
 	/* (non-Javadoc)
@@ -276,6 +282,8 @@ public class FiscalizacaoConcluidaActivity extends AnalyticsActivity
 	protected void onResume() 
 	{
 		super.onResume();
+		
+		uiHelper.onResume();
 
 		Uri uri = getIntent().getData(); 
 		if (uri != null)
@@ -290,34 +298,58 @@ public class FiscalizacaoConcluidaActivity extends AnalyticsActivity
 		}				
 	}
 
+
+
+	/* (non-Javadoc)
+	 * @see android.app.Activity#onPause()
+	 */
 	@Override
-	public void onStart() 
-	{
-		super.onStart();
-		Session.getActiveSession().addCallback(statusCallback);
+	protected void onPause() 
+	{		
+		super.onPause();
+		
+		uiHelper.onPause();
 	}
+	
+	
 
-
+	/* (non-Javadoc)
+	 * @see android.app.Activity#onDestroy()
+	 */
 	@Override
-	public void onStop() 
-	{   
-		super.onStop();
-		Session.getActiveSession().removeCallback(statusCallback);        
+	protected void onDestroy() 
+	{	
+		super.onDestroy();
+		
+		uiHelper.onDestroy();
 	}
 
 	@Override
 	protected void onSaveInstanceState(Bundle outState) 
 	{
 		super.onSaveInstanceState(outState);
-		Session session = Session.getActiveSession();
-		Session.saveSession(session, outState);
+		uiHelper.onSaveInstanceState(outState);
 	}	
 
 	@Override
 	public void onActivityResult(int requestCode, int resultCode, Intent data) 
 	{
 		super.onActivityResult(requestCode, resultCode, data);
-		Session.getActiveSession().onActivityResult(this, requestCode, resultCode, data);
+		
+		 uiHelper.onActivityResult(requestCode, resultCode, data, new FacebookDialog.Callback() 
+		 {
+		        @Override
+		        public void onError(FacebookDialog.PendingCall pendingCall, Exception error, Bundle data) 
+		        {
+		        	Toast.makeText(FiscalizacaoConcluidaActivity.this.getApplicationContext(),"Erro ao compartilhar no Facebook!",Toast.LENGTH_SHORT).show();
+		        }
+
+		        @Override
+		        public void onComplete(FacebookDialog.PendingCall pendingCall, Bundle data) 
+		        {
+		        	Toast.makeText(FiscalizacaoConcluidaActivity.this.getApplicationContext(),"Compartilhado com sucesso!",Toast.LENGTH_SHORT).show();
+		        }
+		    });
 	}
 
 	/**
@@ -421,47 +453,6 @@ public class FiscalizacaoConcluidaActivity extends AnalyticsActivity
 		RequestAsyncTask task = new RequestAsyncTask(request);
 		task.execute();
 	}
-
-
-	private class SessionStatusCallback implements Session.StatusCallback
-	{ 
-		@Override
-		public void call(Session session, SessionState state, Exception exception) 
-		{
-			if (session.isOpened()) 
-			{							
-				List<String> permissions = session.getPermissions();
-
-				List<String> PERMISSIONS = Arrays.asList("publish_actions");
-
-				if (!isSubsetOf(PERMISSIONS, permissions)) 
-				{
-					Session.NewPermissionsRequest newPermissionsRequest = new Session.NewPermissionsRequest(FiscalizacaoConcluidaActivity.this, PERMISSIONS);
-					session.requestNewPublishPermissions(newPermissionsRequest);
-					return;
-				}
-
-				publishStory(session);
-			}
-		}
-	}
-
-	private boolean isSubsetOf(Collection<String> subset,Collection<String> superset) 
-	{
-		for (String string : subset) 
-		{
-			if (!superset.contains(string)) 
-			{
-				return false;
-			}
-		}
-		return true;
-	}
-
-	public void voltar(View view)
-	{
-		finish();
-	}	
 
 	public boolean startTwitterLogin(final String callbackURL)
 	{		
